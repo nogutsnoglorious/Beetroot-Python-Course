b59cea7a44ecf119d504673e6acde0f79ebd1be5
Valentyn Kryvoruchenko <krivoruchenko.valentine@gmail.com>
2023-11-10 20:13:27 +0100

    Completely new code

--- Lesson_04/hw_04-task_1.py
+++ Lesson_04/hw_04-task_1.py
@@ -1,3 +1,18 @@
+while True:
+    word = input("Enter a desired word (f.e. 'helloworld', 'my', 'x', etc): ")
+    if word == "":
+        break
+    elif len(word) >= 2:
+        print(word[:2] + word[-2:])
+    else:
+        print("")
+ 
+
+
+
+# early version of code
+
+"""
 # held
 sample_1 = 'helloworld'
 print(sample_1[:2]+sample_1[-2:])
@@ -10,6 +25,4 @@ print(sample_2[:2]+sample_2[-2:])
 sample_3 = 'x'
 if len(sample_3) < 2:
     print("")
-
-
-
+"""
