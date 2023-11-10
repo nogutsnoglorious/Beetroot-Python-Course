37e598e808aa52445da8eb60318dd88269f0a0dd
Valentyn Kryvoruchenko <krivoruchenko.valentine@gmail.com>
2023-11-10 20:02:36 +0100

    Changed code

--- Lesson_03/hw_03-task_1.py
+++ Lesson_03/hw_03-task_1.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 name = "Valentyn"
 today = datetime.now().strftime('%A')
-day = "Tuesday"
 
 print(f'Good day {name}! {today} is a perfect day to learn some Python.')
 
 # second option
 
-print("Good day %s! %s is a perfect day to learn some Python." % (name, day))
+print("Good day %s! %s is a perfect day to learn some Python." % (name, today))
