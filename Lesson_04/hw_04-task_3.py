97fdc05bddeb46bf96632022d5f0956ec43897cd
Valentyn Kryvoruchenko <krivoruchenko.valentine@gmail.com>
2023-11-10 20:30:14 +0100

    Change the whole code

--- Lesson_04/hw_04-task_3.py
+++ Lesson_04/hw_04-task_3.py
@@ -1,13 +1,16 @@
-exit = False
-while exit == False:
-    answer = input("What is the answer for this expression? 2 + 2 * 2 = ... ")
-    if answer.isdigit():
-        if int(answer) == 6:
+import random
+while True:
+    a = random.randint(1,11)
+    b = random.randint(1,11)
+    answer = input(f'What is the answer for this expression? {a} + {a} * {b} = ... ')
+    if answer == "":
+        break
+    elif answer.isdigit():
+        if int(answer) == a + a * b:
             print("You are correct!")
-            exit = True
+            continue
         else:
             print("Wrong! Try again.")
+            continue
     else:
         print ("Wrong! An answer should be numerical. Try again.")
-
-
