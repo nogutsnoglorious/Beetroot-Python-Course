34e97d5a0e6410280910c0e20cb7772246aa5ce8
Valentyn Kryvoruchenko <krivoruchenko.valentine@gmail.com>
2023-11-10 20:41:16 +0100

    Changed code to a more practical version

--- Lesson_05/hw_05-task_1.py
+++ Lesson_05/hw_05-task_1.py
@@ -2,13 +2,17 @@ import random
 print('Greetings! Lets play "Guess A Number" game.')
 
 while True:
-    user_number = int(input('Enter any number in the range from 1 to 10: '))
     comp_number = random.randint(1,10)
-    if user_number == "exit":
-        break
-    elif user_number == comp_number:
-        print("Congrats! You win!")
-    elif user_number > 10 or user_number < 1:
-        print("Entered number is out of range.")
-    else:
-        print("Sorry, you lost!")
+    user_number = input('Enter any number in the range from 1 to 10: ')
+    if user_number == "":
+            break
+    elif type(int(user_number)) != int:
+        print("It is a character, not a number.")
+        continue
+    else:        
+        if user_number == comp_number:
+            print("Congrats! You win!")
+        elif user_number > 10 or user_number < 1:
+            print("Entered number is out of range.")
+        else:
+            print("Sorry, you lost!")
