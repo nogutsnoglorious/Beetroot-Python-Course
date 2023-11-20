a5a0d0ecc119a0a592aefbab21daa48e6ded8c9f
Valentyn Kryvoruchenko <krivoruchenko.valentine@gmail.com>
2023-11-20 16:58:15 +0100

    Initial commit

---
+++ Lesson_09/hw_09-task_02.py
@@ -0,0 +1,7 @@
+# Yes, we can change PYTHONPATH variable from sys.path, which is represented as a list.
+# We can write in terminal the next sequence of code:
+
+# python - to activate an environment
+# import sys - to import sys module
+# print(sys.path) - to get the list of folders which Python is looking through while searching necessary information
+# sys.path.append('path\\to\\necessary\\folder') - to add new folder to a list
