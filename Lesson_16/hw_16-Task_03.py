37494e699a66eac5c07fbc0535d89d22a916c45c
Valentyn Kryvoruchenko <krivoruchenko.valentine@gmail.com>
2023-12-15 19:41:16 +0100

    All done

--- Lesson_16/hw_16-Task_03.py
+++ Lesson_16/hw_16-Task_03.py
@@ -18,33 +18,92 @@
 # - get_product_info(product_name) - returns a tuple with product name and amount of items in the store.
 
 class Product:
-    def __init__(self, type, name, price):
-        self.type = type
+    def __init__(self, prod_type, name, price):
+        self.prod_type = prod_type
         self.name = name
         self.price = price
 
-class ProductStore(Product):
-    def __init__(self, type, name, price, amount, discount):
-        super().__init__(type, name, price)
-        self.amount = amount
-        self.discount = discount
+class ProductStore:
+    store_database = []
+    predef_price = 1.3      # 30 percent price premium
+    income = 0
+
+    def add(self, product, quantity):
+        new_product = {
+            "Type": product.prod_type,
+            "Name": product.name,
+            "Price": product.price,
+            "Quantity": quantity
+        }
+        
+        self.store_database.append(new_product)
     
-    def add(self, product, amount):
-        if product_store_base == []:
-            product_store_base.append(product)
-            for el in product_store_base:
-                for key, value in el:
-                    if key == self.name:
-                        value = value + amount
-                        break
-        else:
-            for el in product_store_base:
-                for key, value in el:
-                    if key == self.name:
-                        value = value + amount
-                        break
+    def show_database(self):
+        for product in self.store_database:
+            print(product)
+        print("*"*100, "\n")
 
+    def set_discount(self, name_or_type, discount):
+        for el in self.store_database:
+            if el["Type"] == name_or_type:
+                el.update({"Discount": str(discount) + "%"})
+            elif el["Name"] == name_or_type:
+                el.update({"Discount": str(discount) + "%"})
+    
+    def sell_product(self, prod_type, amount):
+        for el in self.store_database:
+            if "Discount" in el and el["Discount"]:
+                if el["Type"] == prod_type:
+                    if el["Quantity"] >= amount:
+                        el["Quantity"] -= amount
+                        print(f'{el["Discount"]} discount is applied!')
+                        discount = int(el["Discount"].replace("%","")) / 100
+                        self.income += amount * el["Price"] * self.predef_price * (1 - discount)
+                        print(f'We\'ve just sold {amount} {el["Type"]} and our total income is {self.income}.\n{"*"*100}\n')
+                    else:
+                        raise ValueError ("There's not enough items in the warehouse for this operation")
+            else:
+                if el["Type"] == prod_type:
+                    if el["Quantity"] >= amount:
+                        el["Quantity"] -= amount
+                        self.income += amount * el["Price"] * self.predef_price
+                        print(f'We\'ve just sold {amount} {el["Type"]} and our total income is {self.income}.\n{"*"*100}\n')
+                    else:
+                        raise ValueError ("There's not enough items in the warehouse for this operation")
+    
+    def get_income(self):
+        print(f'Total income at this moment is ${self.income}.\n{"*"*100}\n')
 
-product_store_base = []
-product_1 = Product(type="shoes", name="Nike", price=100, amount=5, discount="10%")
-ProductStore.add(ProductStore, product_1, 10)
+    def get_all_products(self):                 # get only product that are available for sale
+        for product in self.store_database:
+            if product["Quantity"] > 0:
+                print(product)
+        print("*"*100, "\n")
+    
+    def get_product_info(self, name):
+        for product in self.store_database:
+            if product["Name"] == name:
+                for key, value in product.items():
+                    print(f'{key}: {value}')
+        print("*"*100, "\n")
+
+shoes = Product('shoes', 'Nike', 20)
+glasses = Product('glasses', 'Ray-Ban', 10)
+jeans = Product('jeans', 'Levis', 9)
+
+store = ProductStore()
+store.add(shoes, 5)
+store.add(glasses, 11)
+store.show_database()
+store.add(jeans, 3)
+store.get_income()
+store.set_discount('shoes', 30)
+store.set_discount('Levis', 5)
+store.show_database()
+store.sell_product('glasses', 9)
+store.show_database()
+store.sell_product('shoes', 5)
+store.sell_product('jeans', 2)
+store.get_all_products()
+store.show_database()
+store.get_product_info('Levis')
